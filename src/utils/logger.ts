58a373b3cc04e329fc9450218e5bcaabf789a8ec
Edson Cândido <edson.c.a.j@hotmail.com>
2024-02-27 00:55:14 -0300

    [feat] formato de data do log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,10 @@
 import pino from "pino";
 
-
 export default pino({
     enabled: true,
     level: "info",
     // timestamp: pino.stdTimeFunctions.isoTime,
-    timestamp: () => `, "timestamp":"${(new Date())}"`,
+    timestamp: () => `, "timestamp":"${new Date().toLocaleString('pt-br')}"`,
     formatters: {
         level: (label) => {
             return { severity: label.toUpperCase() };
